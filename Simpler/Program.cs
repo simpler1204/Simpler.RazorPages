8d77931defbedbb02c631dc72e657235973ef97a
simpler <suwoni7335@naver.com>
2023-01-25 13:02:49 +0800

    Add Razor page from bootstrap

--- Simpler/Program.cs
+++ Simpler/Program.cs
@@ -4,9 +4,12 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-
-app.MapRazorPages();
-app.MapGet("/", () => "Hello World!!!");
+// 미들웨어 추가
+app.MapRazorPages();  // pages를 사용하기 위해
+app.UseStaticFiles(); // wwwroot를 사용하기 위해(정적인 html, css, javascript,.. 실행하고 싶을때)
+//app.UseFileServer();
+//app.UseRouting();
+//app.MapGet("/", () => "Hello World!!!");
 
 
 app.Run();
