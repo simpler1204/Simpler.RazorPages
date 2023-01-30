2e4292cd51682957880473dacf6303b27a873ceb
simpler <suwoni7335@naver.com>
2023-01-30 16:26:17 +0800

    포트폴리오 리스트를 Blazor Component  사용출력

--- Simpler/Program.cs
+++ Simpler/Program.cs
@@ -4,6 +4,7 @@ using Simpler.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
 //Di(의존성 주입)
 //PortfolioServiceJsonFile에는
@@ -13,7 +14,9 @@ var app = builder.Build();
 
 // 미들웨어 추가
 app.MapRazorPages();  // pages를 사용하기 위해
+app.MapBlazorHub();
 app.UseStaticFiles(); // wwwroot를 사용하기 위해(정적인 html, css, javascript,.. 실행하고 싶을때)
+
 //app.UseFileServer();
 //app.UseRouting();
 //app.MapGet("/", () => "Hello World!!!");
